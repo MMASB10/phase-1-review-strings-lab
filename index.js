6a5743c3a337c24f5e3ffb611fa03e8263774c9f
Muhammad <muhammadbakhshuwein10@gmail.com>
2025-09-10 20:49:46 +0300

    complete strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = ' Theodore Bartholomew';
+
+const welcomeMessage = `Welcome to Flatbook, + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
